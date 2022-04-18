896e9472e5f040d79b40ef96cd68fb7cd81e9a8d
adisbladis <adisbladis@gmail.com>
2022-04-18 19:13:59 +1200

    Add back a boilerplate flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Build Nix bootstraping packages for legacy distributions";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, flake-utils }: (
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = import ./. { inherit pkgs; };
+      }
+    )
+  );
+
+}
