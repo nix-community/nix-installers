5fa5278128aba98ebfc87912bb984971c0932d84
adisbladis <adisbladis@gmail.com>
2022-04-21 01:33:04 +1200

    Add cross compiled aarch64 packages to prebuilt

--- .github/pages.nix
+++ .github/pages.nix
@@ -2,14 +2,33 @@
     let
       flakeLock = builtins.fromJSON (builtins.readFile ../flake.lock);
     in
-      import "${builtins.fetchTree flakeLock.nodes.nixpkgs.locked}" { }
+    import "${builtins.fetchTree flakeLock.nodes.nixpkgs.locked}" { }
   )
 , lib ? pkgs.lib
 }:
 
+let
+  systems = {
+    "x86_64" = pkgs;
+    "aarch64" = pkgs.pkgsCross.aarch64-multiplatform;
+  };
+  formats = lib.attrNames (import ../. { inherit pkgs lib; });
+
+  # Map (format -> arch -> drv)
+  # Example: deb -> aarch64 -> <<derivation: foo>>
+  installers = lib.listToAttrs (map
+    (
+      fmt: {
+        name = fmt;
+        value = lib.mapAttrs (_: pkgs: (import ../. { inherit pkgs lib; }).${fmt}) systems;
+      }
+    )
+    formats);
+
+in
 pkgs.runCommand "github-pages"
 {
-  installers = import ../. { inherit pkgs lib; };
+  inherit installers;
   __structuredAttrs = true;
   PATH = "${pkgs.coreutils}/bin:${pkgs.python3}/bin:${pkgs.pandoc}/bin";
   builder = builtins.toFile "builder"
@@ -17,5 +36,5 @@ pkgs.runCommand "github-pages"
       . .attrs.sh
       python3 ${./pages.py} ${../README.md} .attrs.json ''${outputs[out]}
     '';
-  }
+}
   ""
