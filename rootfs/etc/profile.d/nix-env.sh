42b45b550e80a03dc8290cb128bbc252e352eee3
Dusk Banks <me@bb010g.com>
2023-07-05 18:23:39 -0700

    fix: prevent child shells sourcing `nix-env.sh`
    This sourcing guard is based on NixOS's sourcing guard. Note that
    NixOS's guard uses the environment variable
    `$__NIXOS_SET_ENVIRONMENT_DONE`, while this guard uses a new variable,
    `$__NIX_DAEMON_SET_ENVIRONMENT_DONE`, that matches existing Nix & NixOS
    naming schemes.

--- rootfs/etc/profile.d/nix-env.sh
+++ rootfs/etc/profile.d/nix-env.sh
@@ -1,40 +1,50 @@
 #!/bin/sh
-# Avoid duplication of `PATH` entries.
-while :; do case "${PATH-}:" in
-	${HOME:+"$HOME/.nix-profile/bin":*})
-		PATH="${PATH}:"
-		PATH="${PATH#$HOME/.nix-profile/bin:}"
-		PATH="${PATH%:}"
-		;;
-	"/nix/var/nix/profiles/system/bin":*)
-		PATH="${PATH}:"
-		PATH="${PATH#/nix/var/nix/profiles/system/bin:}"
-		PATH="${PATH%:}"
-		;;
-	"/nix/var/nix/profiles/default/bin":*)
-		PATH="${PATH}:"
-		PATH="${PATH#/nix/var/nix/profiles/default/bin:}"
-		PATH="${PATH%:}"
-		;;
-	*) break ;;
-esac; done
-# Avoid duplication of `XDG_DATA_DIRS` entries.
-while :; do case "${XDG_DATA_DIRS-}:" in
-	${HOME:+"$HOME/.nix-profile/share/":*})
-		XDG_DATA_DIRS="${XDG_DATA_DIRS}:"
-		XDG_DATA_DIRS="${XDG_DATA_DIRS#$HOME/.nix-profile/share/:}"
-		XDG_DATA_DIRS="${XDG_DATA_DIRS%:}"
-		;;
-	"/nix/var/nix/profiles/system/share/":*)
-		XDG_DATA_DIRS="${XDG_DATA_DIRS}:"
-		XDG_DATA_DIRS="${XDG_DATA_DIRS#/nix/var/nix/profiles/system/share/:}"
-		XDG_DATA_DIRS="${XDG_DATA_DIRS%:}"
-		;;
-	"/nix/var/nix/profiles/default/share/":*)
-		XDG_DATA_DIRS="${XDG_DATA_DIRS}:"
-		XDG_DATA_DIRS="${XDG_DATA_DIRS#/nix/var/nix/profiles/default/share/:}"
-		XDG_DATA_DIRS="${XDG_DATA_DIRS%:}"
-		;;
-	*) break ;;
-esac; done
-. /usr/lib/environment.d/nix-daemon.conf
+# Prevent being sourced from child shells.
+if [ -z "$__NIX_DAEMON_SET_ENVIRONMENT_DONE" ]; then
+	# Avoid duplication of `PATH` entries.
+	while :; do case "${PATH-}:" in
+		${HOME:+"$HOME/.nix-profile/bin":*})
+			PATH="${PATH}:"
+			PATH="${PATH#$HOME/.nix-profile/bin:}"
+			PATH="${PATH%:}"
+			;;
+		"/nix/var/nix/profiles/system/bin":*)
+			PATH="${PATH}:"
+			PATH="${PATH#/nix/var/nix/profiles/system/bin:}"
+			PATH="${PATH%:}"
+			;;
+		"/nix/var/nix/profiles/default/bin":*)
+			PATH="${PATH}:"
+			PATH="${PATH#/nix/var/nix/profiles/default/bin:}"
+			PATH="${PATH%:}"
+			;;
+		*) break ;;
+	esac; done
+	# Avoid duplication of `XDG_DATA_DIRS` entries.
+	while :; do case "${XDG_DATA_DIRS-}:" in
+		${HOME:+"$HOME/.nix-profile/share/":*})
+			XDG_DATA_DIRS="${XDG_DATA_DIRS}:"
+			XDG_DATA_DIRS="${XDG_DATA_DIRS#$HOME/.nix-profile/share/:}"
+			XDG_DATA_DIRS="${XDG_DATA_DIRS%:}"
+			;;
+		"/nix/var/nix/profiles/system/share/":*)
+			XDG_DATA_DIRS="${XDG_DATA_DIRS}:"
+			XDG_DATA_DIRS="${XDG_DATA_DIRS#/nix/var/nix/profiles/system/share/:}"
+			XDG_DATA_DIRS="${XDG_DATA_DIRS%:}"
+			;;
+		"/nix/var/nix/profiles/default/share/":*)
+			XDG_DATA_DIRS="${XDG_DATA_DIRS}:"
+			XDG_DATA_DIRS="${XDG_DATA_DIRS#/nix/var/nix/profiles/default/share/:}"
+			XDG_DATA_DIRS="${XDG_DATA_DIRS%:}"
+			;;
+		*) break ;;
+	esac; done
+	# Ensure environment variables are exported.
+	if [ "$-" != "${-%%a*}" ]; then
+		. /usr/lib/environment.d/nix-daemon.conf
+	else
+		set -a
+		. /usr/lib/environment.d/nix-daemon.conf
+		set +a
+	fi
+fi
